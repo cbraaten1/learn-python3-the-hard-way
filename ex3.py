fe0e3d48e0fbf216769ae23d084ea9153558b433
cbraaten1 <95046227+cbraaten1@users.noreply.github.com>
2023-01-12 21:28:55 -0600

    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print('I will now count my chickens:')
+
+print('Hens', 25 + 30 / 6) # Utilizing addition and division.
+print('Roosters', 100 -  25 * 3 % 4) # Utilizing subtraction, multiplication and dividing the remaining numbers.
+
+print('Now I will count the eggs:')
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Combining all fo the operators into one problem.
+
+print('Is it true that 3 + 2 < 5 - 7?')
+
+print(3 + 2 < 5 - 7) # Comparing two numbers if one is greater then the other.
+
+print('What is 3 + 2?', 3 + 2) # Breaking down the problem with addition.
+print('What is 5 - 7?', 5 - 7) # Breaking down the problem with subtractions.
+
+print("Oh, that's why it's False.")
+
+print('How about some more.')
+
+print('Is it greater?', 5 > 2) # Asking the question is 5 greater then 2.
+print('Is it greater or equal?', 5 >= -2) # Asking the question is 5 greater then or equal -2.
+print('Is it less or equal?', 5 <= -2) # Asking the question is 5 less then or equal to -2.
